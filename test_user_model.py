09a1bf2fcf77b48ef4947d7c748c5b8fac5f6c1e
Jarret Berman <jarretberman11@gmail.com>
2020-07-12 11:24:46 -0500

    user model tests

--- test_user_model.py
+++ test_user_model.py
@@ -56,3 +56,105 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_signup_user(self):
+        """Does signup class method work?"""
+
+        u = User.signup(
+            username='testuser',
+            email='test@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        #User should have no messages & no followers
+        self.assertEqual(len(u.messages), 0)
+        self.assertEqual(len(u.followers), 0)
+        #User password should be encrypted and not stored as entered password
+        self.assertNotEqual('testtest', u.password)
+
+        u2 = User.signup(
+            username='testuser',
+            email='test2@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+        db.session.add(u2)
+
+        #Raises error when a non-unique username is committed to the db
+        self.assertRaises(IntegrityError,db.session.commit())
+
+    def test_authenicate_user(self):
+        """Does authenticate class method work"""
+
+        u = User.signup(
+            username='testuser',
+            email='test@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        #Properly authenticates when password is correct
+        self.assertEqual(u, User.authenticate(username='testuser',password='testtest'))
+        #Returns false if it can't authenticate
+        self.assertFalse(User.authenticate(username='testuser',password='wrongpassword'))
+
+    def test_is_following(self):
+        """Does is_following method work"""
+
+        u = User.signup(
+            username='testuser',
+            email='test@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+        u2 = User.signup(
+            username='testuser2',
+            email='test2@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+        db.session.add_all([u,u2])
+        db.session.commit()
+
+        #Should return false when the users are not following eachother
+        self.assertFalse(u.is_following(u2))
+
+        u.following.append(u2)
+        db.session.commit()
+
+        #Should return true when a user is following another
+        self.assertTrue(u.is_following(u2))
+
+    def test_is_followed_by(self):
+
+        u = User.signup(
+            username='testuser',
+            email='test@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+        u2 = User.signup(
+            username='testuser2',
+            email='test@test.com',
+            password= 'testtest',
+            image_url= '/test/url',
+        )
+        
+        db.session.add_all([u,u2])
+        db.session.commit()
+
+        #Should return false when the users are not following eachother
+        self.assertFalse(u.is_followed_by(u2))
+
+        u.following.append(u2)
+        db.session.commit()
+
+        #Should return true when a user is following another
+        self.assertTrue(u2.is_followed_by(u))
